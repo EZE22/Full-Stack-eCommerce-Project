79e20d321701aabef1cf95bb4adf8c7e44701195
Adam Ingram <everett.ingram22@gmail.com>
2020-05-28 17:12:11 -0400

    Update Angular Component to subscribe

--- backend/src/main/java/com/jandacoding/springbootecommerce/dao/ProductRepository.java
+++ backend/src/main/java/com/jandacoding/springbootecommerce/dao/ProductRepository.java
@@ -3,7 +3,9 @@ package com.jandacoding.springbootecommerce.dao;
 
 import com.jandacoding.springbootecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-                                                    //Entity Product and Primary key
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }

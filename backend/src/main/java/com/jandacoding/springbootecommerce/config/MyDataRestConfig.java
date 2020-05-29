9a76c17ae4fae81dd290201c8316e7c5943e85d0
Adam Ingram <everett.ingram22@gmail.com>
2020-05-29 13:19:33 -0400

    Updated the back-end

--- backend/src/main/java/com/jandacoding/springbootecommerce/config/MyDataRestConfig.java
+++ backend/src/main/java/com/jandacoding/springbootecommerce/config/MyDataRestConfig.java
@@ -9,23 +9,23 @@ import org.springframework.http.HttpMethod;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
-    @Override
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+//    @Override
+//    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
 
-        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
-
-        //dissable HTTP methods for product: PUT, POST and DELETE
-        //This make the project READ ONLY
-        config.getExposureConfiguration()
-                .forDomainType(Product.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
-
-        //dissable HTTP methods for productCategory: PUT, POST and DELETE
-        //This make the project READ ONLY
-        config.getExposureConfiguration()
-                .forDomainType(ProductCategory.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
-    }
+//        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+//
+//        //dissable HTTP methods for product: PUT, POST and DELETE
+//        //This make the project READ ONLY
+//        config.getExposureConfiguration()
+//                .forDomainType(Product.class)
+//                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+//                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+//
+//        //dissable HTTP methods for productCategory: PUT, POST and DELETE
+//        //This make the project READ ONLY
+//        config.getExposureConfiguration()
+//                .forDomainType(ProductCategory.class)
+//                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+//                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+//    }
 }
